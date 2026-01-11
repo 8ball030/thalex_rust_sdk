0f777225dc09335cac7645a4aa84c5d847a26fa4
8ball030 <8baller@station.codes>
2026-01-11 10:18:54 +0000

    feat:added-test-for-bot

---
+++ tests/bot_test.rs
@@ -0,0 +1,28 @@
+mod common;
+use thalex_rust_sdk::{models::{CreateBotParams, DHedge1}, ws_client::WsClient};
+
+no_params_private_rpc_test!(
+    test_bot_get_bots,
+    bots,
+    "Bot get_bot",
+    bot
+);
+
+
+fn get_test_hedge_strategy() -> DHedge1 {
+    DHedge1 {
+        strategy: "dhedge".to_string(),
+        period: 3600.0,
+        instrument_name: "ETH-PERPETUAL".to_string(),
+        ..Default::default()
+    }
+}
+
+
+params_private_rpc_test!(
+    test_bot_create_dhedge_bot,
+    CreateBotParams::DHedge1(get_test_hedge_strategy()),
+    create_bot,
+    "Bot create dhedge bot",
+    bot
+);
